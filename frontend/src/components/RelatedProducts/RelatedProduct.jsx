d136263683dd8d919c2a7d738ffeedbaf3edaa6e
abdelmajid200 <133158310+abdelmajid200@users.noreply.github.com>
2024-08-25 23:09:45 +0100

    Update RelatedProduct.jsx

--- frontend/src/components/RelatedProducts/RelatedProduct.jsx
+++ frontend/src/components/RelatedProducts/RelatedProduct.jsx
@@ -6,7 +6,7 @@ const RelatedProduct = () => {
   const [new_collection,setNew_collection] = useState([])
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://preject-final-backend.onrender.com/newcollections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
